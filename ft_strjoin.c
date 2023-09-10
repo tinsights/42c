b2e6aa0d160cd02db8738ae17e10ded2eeba8e18
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2023-09-10 15:15:30 +0800

    ft_strtrim

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,32 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tjegades <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/09 14:33:35 by tjegades          #+#    #+#             */
-/*   Updated: 2023/09/09 14:33:36 by tjegades         ###   ########.fr       */
+/*   Created: 2023/09/10 14:41:46 by tjegades          #+#    #+#             */
+/*   Updated: 2023/09/10 14:41:47 by tjegades         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, char *src, size_t size)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	len;
+	int		len;
+	int		i;
+	char	*result;
 
-	len = ft_strlen(src);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	result = malloc(len + 1);
+	if (!result)
+		return (NULL);
+	result[len] = 0;
 	i = 0;
-	if (size)
-	{
-		while (src[i] && i + 1 < size)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (len);
+	while (*s1)
+		*(result + i++) = *s1++;
+	while (*s2)
+		*(result + i++) = *s2++;
+	return (result);
 }
