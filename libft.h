eea76587f3dcd1cb2b3fed3cb41e71b28ea15f63
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2023-09-10 17:41:03 +0800

    ft_putchar_fd.c

--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 # define INT_MIN -2147483648
 
@@ -47,5 +48,6 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
 
 #endif
